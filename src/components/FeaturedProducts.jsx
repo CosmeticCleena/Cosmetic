314af6a5a7137023aa31c7ffa1816c65daec1e41
Tung Pho <ductungpho1005@gmail.com>
2025-03-29 20:46:56 +0700

    update Responsive

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -3,8 +3,8 @@ import FeaturedProductCard from "./FeaturedProductCard";
 
 const FeaturedProducts = () => {
   return (
-    <div className="w-full bg-white max-h-[738px] font-magnificent md:py-[60px] md:px-[120px]">
-      <h1 className="text-[40px] text-center">CÁC SẢN PHẨM NỔI BẬT</h1>
+    <div className="w-full bg-white py-[60px] font-magnificent md:px-[120px]">
+      <h1 className="text-[40px] text-center mb-8">CÁC SẢN PHẨM NỔI BẬT</h1>
       <div className="md:flex grid grid-cols-2 gap-[30px] justify-center p-4">
         <FeaturedProductCard />
         <FeaturedProductCard />
@@ -13,5 +13,4 @@ const FeaturedProducts = () => {
     </div>
   );
 };
-
 export default FeaturedProducts;
