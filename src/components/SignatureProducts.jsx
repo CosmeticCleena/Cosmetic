7449df24fb8ad31b901660ab3dc588038c3dacdd
Vũ Minh Hiếu <vuminhhieu171103@gmail.com>
2025-03-29 22:51:20 +0700

    update signature product

--- src/components/SignatureProducts.jsx
+++ src/components/SignatureProducts.jsx
@@ -17,7 +17,7 @@ const SignatureProducts = () => {
       />
 
       <div className="relative z-10 px-4 py-8 md:py-12">
-        <div className="w-full md:w-[75%] mx-auto">
+        <div className="w-full md:w-[76%] mx-auto">
           <p className="font-magnificent text-2xl text-[#E6E6E6] md:text-3xl w-full md:w-[70%] lg:w-[50%] text-start mb-8">
             Các dòng sản phẩm tiêu biểu của chúng tôi
           </p>
