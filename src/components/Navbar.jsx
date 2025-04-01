c500e2f8a6c16c99a21cddc5072bcbcbd83fa05a
Vũ Minh Hiếu <vuminhhieu171103@gmail.com>
2025-04-01 13:23:32 +0700

    add navbar mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,64 +1,76 @@
+import { useState } from "react";
 import React from "react";
 import LumiaLogo from "../assets/images/LumiaLogo.svg";
 import SearchIcon from "../assets/icons/SearchIcon.svg";
 import hamburgerMenu from "../assets/icons/hamburgerMenu.svg";
 import User from "../assets/icons/User.svg";
 import Cart from "../assets/icons/Cart.svg";
+import SideBarMobile from "./SidebarMobile";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="h-[96px] w-full">
-      <div className="px-4 md:px-[120px] pt-2 pb-2 flex items-center justify-between">
-        <img className="w-[150px] md:w-[200px]" src={LumiaLogo} alt="logo" />
+    <>
+      <nav className="h-[96px] w-full bg-white fixed top-0 z-50">
+        <div className="px-4 md:px-[120px] pt-2 pb-2 flex items-center justify-between">
+          <img className="w-[150px] md:w-[200px]" src={LumiaLogo} alt="logo" />
 
-        {/* Desktop Navigation */}
-        <div className="hidden md:flex items-center justify-between">
-          <ul className="flex items-center gap-4 mr-8 font-magnificent">
-            <li className="text-transparent bg-clip-text bg-gradient-to-r cursor-pointer from-[#B08B3B] to-[#EAC980] whitespace-nowrap">
-              Giới thiệu
-            </li>
-            <li className="whitespace-nowrap cursor-pointer">Về chúng tôi</li>
-            <li className="whitespace-nowrap cursor-pointer">Sản phẩm</li>
-            <li className="whitespace-nowrap cursor-pointer">Liên hệ</li>
-          </ul>
+          {/* Desktop Navigation */}
+          <div className="hidden md:flex items-center justify-between">
+            <ul className="flex items-center gap-4 mr-8 font-magnificent">
+              <li className="text-transparent bg-clip-text bg-gradient-to-r cursor-pointer from-[#B08B3B] to-[#EAC980] whitespace-nowrap">
+                Giới thiệu
+              </li>
+              <li className="whitespace-nowrap cursor-pointer">Về chúng tôi</li>
+              <li className="whitespace-nowrap cursor-pointer">Sản phẩm</li>
+              <li className="whitespace-nowrap cursor-pointer">Liên hệ</li>
+            </ul>
 
-          <div className="flex items-center gap-3">
-            <div className="w-5 h-5 flex items-center justify-center cursor-pointer">
-              <img
-                src={SearchIcon}
-                alt="search-icon"
-                className="w-full h-full object-contain"
-              />
+            <div className="flex items-center gap-3">
+              <div className="w-5 h-5 flex items-center justify-center cursor-pointer">
+                <img
+                  src={SearchIcon}
+                  alt="search-icon"
+                  className="w-full h-full object-contain"
+                />
+              </div>
+              <div className="w-5 h-5 flex items-center justify-center cursor-pointer">
+                <img
+                  src={User}
+                  alt="user-icon"
+                  className="w-full h-full object-contain"
+                />
+              </div>
+              <div className="w-5 h-5 flex items-center justify-center cursor-pointer">
+                <img
+                  src={Cart}
+                  alt="cart-icon"
+                  className="w-full h-full object-contain"
+                />
+              </div>
             </div>
-            <div className="w-5 h-5 flex items-center justify-center cursor-pointer">
+          </div>
+
+          {/* Mobile Navigation - only hamburger menu */}
+          <div className="md:hidden cursor-pointer" onClick={toggleMenu}>
+            <div className="w-6 h-6 flex items-center justify-center ">
               <img
-                src={User}
-                alt="user-icon"
-                className="w-full h-full object-contain"
-              />
-            </div>
-            <div className="w-5 h-5 flex items-center justify-center cursor-pointer">
-              <img
-                src={Cart}
-                alt="cart-icon"
+                src={hamburgerMenu}
+                alt="menu"
                 className="w-full h-full object-contain"
               />
             </div>
           </div>
         </div>
+      </nav>
 
-        {/* Mobile Navigation - only hamburger menu */}
-        <div className="md:hidden cursor-pointer">
-          <div className="w-6 h-6 flex items-center justify-center ">
-            <img
-              src={hamburgerMenu}
-              alt="menu"
-              className="w-full h-full object-contain"
-            />
-          </div>
-        </div>
-      </div>
-    </nav>
+      <SideBarMobile isOpen={isOpen} />
+    </>
   );
 };
 
---
+++ src/components/SidebarMobile.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { useState } from "react";
+import LeftArrowNav from "../assets/icons/LeftArrowNav.svg";
+import RightArrowNav from "../assets/icons/RightArrowNav.svg";
+import SIDEBAR_DATA from "../configs/Sidebar.json";
+
+const SidebarMobile = ({ isOpen }) => {
+  const [currentMenu, setCurrentMenu] = useState([]);
+
+  const handleMenuClick = (menu) => {
+    setCurrentMenu((prev) => [menu, ...prev]);
+  };
+
+  const handleBackClick = () => {
+    setCurrentMenu((prev) => prev.slice(1));
+  };
+
+  const menuData =
+    currentMenu.length > 0
+      ? SIDEBAR_DATA.find(
+          (item) => item.name === currentMenu[currentMenu.length - 1]
+        ).submenu
+      : SIDEBAR_DATA;
+
+  return (
+    <aside
+      className={`fixed w-screen h-full bg-[#FAF9F5] z-40 pt-20 scroll-auto transition-all duration-300 ease-in-out ${
+        isOpen
+          ? "opacity-100 scale-100"
+          : "opacity-0 scale-95 pointer-events-none"
+      }`}
+    >
+      <div className="w-[90%] mx-auto">
+        <div className="flex items-center space-x-3 my-5 px-4">
+          {currentMenu.length > 0 && (
+            <>
+              <img
+                src={LeftArrowNav}
+                alt="LeftArrow"
+                className="cursor-pointer"
+                onClick={handleBackClick}
+              />
+              <h1 className="font-magnificent text-xl text-[#0C0C0C]">
+                {currentMenu.join(" > ")}
+              </h1>
+            </>
+          )}
+        </div>
+        <div className="w-full flex flex-col transition-all duration-300 ease-in-out">
+          {menuData.map((item, index) => (
+            <div
+              className="bg-white border-t-2 flex py-4 px-4 justify-between border-[#DFDFDF] border-solid hover:bg-gray-100"
+              key={index}
+              onClick={() =>
+                item.submenu?.length > 0 && handleMenuClick(item.name)
+              }
+            >
+              <h1 className="font-magnificent text-xl">{item.name}</h1>
+              {item.submenu.length > 0 && (
+                <img
+                  src={RightArrowNav}
+                  alt="RightArrow"
+                  className="w-6 h-7 object-cover"
+                />
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default SidebarMobile;
