a6b648fdcd86b121c09f3214a58443b6bbdade2c
Vũ Minh Hiếu <106251739+hieu0317@users.noreply.github.com>
2025-04-09 10:28:59 +0700

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import Root from "./pages/Root";
 import Home from "./pages/Home";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import Product from "./pages/Product";
+import ProductDetail from "./pages/ProductDetail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { index: true, element: <Navigate to="home" replace /> },
+      { path: "home", element: <Home /> },
+      { path: "products", element: <Product /> },
+      { path: "products/:id", element: <ProductDetail /> },
+    ],
+  },
+]);
 
 function App() {
-  return <Home />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
