b881b651c00725af955e03d179abffd8da9f691f
Vũ Minh Hiếu <106251739+hieu0317@users.noreply.github.com>
2025-04-09 13:49:24 +0700

    add breadCumbs

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Breadcumbs from "../components/layout/Breadcumbs";
 
 const Product = () => {
-  return <div></div>;
+  return (
+    <div className="bg-[#FAF9F5] w-full h-screen pt-12">
+      <div className="w-[90%] mx-auto">
+        <Breadcumbs />
+      </div>
+    </div>
+  );
 };
 
 export default Product;
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Breadcumbs from "../components/layout/Breadcumbs";
 
 const ProductDetail = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Breadcumbs productName="De Rose Advanced Serum" />
+    </div>
+  );
 };
 
 export default ProductDetail;
